00aeb5357373498c3fd019ff2cc2e514675ea511
BT_1N3_17 <BT_1N3_17@BT_1N3_17.bktech.local>
2017-12-15 15:28:50 -0500

    asdf

---
+++ OrcMathGUI/src/guiPlayer/CatalogMaker.java
@@ -0,0 +1,44 @@
+package guiPlayer;
+
+import java.util.ArrayList;
+
+public class CatalogMaker {
+
+	private ArrayList <Monkey> list;
+	
+	public CatalogMaker() {
+
+		list = new ArrayList<Monkey>();
+		list.add(new Monkey("Blue-Eyes", "Monster", 8, 10));
+		list.add(new Monkey("Neos", "Monster", 7, 8));
+		list.add(new Monkey("Dark Magician", "Monster", 7, 9));
+		list.add(new Monkey("Red-Eyes", "Monster", 7, 6));
+
+		
+	}
+	
+	public static void main(String[] args)
+	{
+		CatalogMaker test = new CatalogMaker();
+		System.out.print(test.getContent());
+	}
+	
+	public String getContent()
+	{
+		String data = "Name,Type,Level,Price\n";
+		
+		
+		for(Monkey i: list)
+		{
+			data += i+"\n";
+		}
+		return data;
+	}
+	
+	public void addNewItem(String name, String type, int level, int price)
+	{
+		list.add(new Monkey(name,type,level,price));
+		System.out.println("Item added Sucessfully");
+	}
+
+}
--- OrcMathGUI/src/guiPlayer/Monkey.java
+++ OrcMathGUI/src/guiPlayer/Monkey.java
@@ -2,28 +2,43 @@ package guiPlayer;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.util.Scanner;
 
 import guiTeacher.components.AnimatedComponent;
 import guiTeacher.components.Component;
 
 public class Monkey extends AnimatedComponent{
 
-
-	public Monkey(){
-		
-		super(40,40,57,69);
-		
-		addSequence("resource/goku.png", 150, 0, 469, 57, 69, 13);
+	private String name;
+	private String type;
+	private int level;
+	private int price;
+	Scanner in = new Scanner(System.in);
 	
-		Thread Animation = new Thread(this);
-		Animation.start();
+	
+	public Monkey(String name, String type, int level, int price){
+		
+		this.name = name;
+		this.type = type;
+		this.level = level;
+		this.price = price;
+		
+		super(40,40,100,100);
+		
+		//addSequence("resources/goku.png", 100, 0, 476, 60, 62, 13);
+	
+		//Thread Animation = new Thread(this);
+		//Animation.start();
 		update();
 		
 		// TODO Auto-generated constructor stub
 	}
 
-
-
+	public String toString()
+	{
+		return name + "," + type + "," + level + "," + price; 
+	}
+	
 
 	@Override
 	public void update(Graphics2D g) {
